4c8634efd7a62c14c47f216b41963ff61510482b
KaijuuYahoo <udinaqimudin@gmail.com>
2026-03-13 15:55:50 +0700

    Testing Terakhir dan merapikan beberapa code

--- PraktikumADO/Form1.cs
+++ PraktikumADO/Form1.cs
@@ -14,9 +14,7 @@ namespace PraktikumADO
         }
         private void Koneksi()
         {
-            conn = new SqlConnection(
-                "Data Source = DESKTOP-C6LEFON\\KAIJUURUN; Initial Catalog = DBAkademikADO; Integrated Security = True"
-                );
+            conn = new SqlConnection("Data Source = DESKTOP-C6LEFON\\KAIJUURUN; Initial Catalog = DBAkademikADO; Integrated Security = True");
         }
 
         //Membuka Koneksi Database 
